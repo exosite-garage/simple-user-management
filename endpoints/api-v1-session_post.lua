2a16ba408ce3fa1124f286b9a8e606a489064619
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-13 15:34:40 -0600

    retab

--- endpoints/api-v1-session_post.lua
+++ endpoints/api-v1-session_post.lua
@@ -1,19 +1,19 @@
 --#ENDPOINT post /api/v1/session application/x-www-form-urlencoded
 -- luacheck: globals request response (magic variables from Murano)
 local ret = User.getUserToken({
-  email = request.body.email,
-  password = request.body.password
+	email = request.body.email,
+	password = request.body.password
 })
 print(to_json(ret))
 if ret.error ~= nil then
-  response.code = 401 --ret.status_code
-  response.message = "Auth failed"
+	response.code = 401 --ret.status_code
+	response.message = "Auth failed"
 else
-  response.headers = {
-    ["Set-Cookie"] = "sid=" .. tostring(ret),
+	response.headers = {
+		["Set-Cookie"] = "sid=" .. tostring(ret),
 		["Location"] = "/",
-  }
-  --response.message = {["token"] = ret}
+	}
+	--response.message = {["token"] = ret}
 	response.code = 303
 end
 -- vim: set ai sw=2 ts=2 :
