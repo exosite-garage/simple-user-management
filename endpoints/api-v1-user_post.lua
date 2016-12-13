c551004794fad8b8c83a3fffc12f5e55548f8245
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-13 15:37:49 -0600

    retab

--- endpoints/api-v1-user_post.lua
+++ endpoints/api-v1-user_post.lua
@@ -5,24 +5,24 @@ local name = request.body.name
 local password = request.body.password
 
 local ret = User.createUser({
-	email = email,
-	name = name or email,
-	password = password
+  email = email,
+  name = name or email,
+  password = password
 })
 if ret.status_code ~= nil then
-	response.code = ret.status_code
-	response.message = ret.message
+  response.code = ret.status_code
+  response.message = ret.message
 else
-	local domain = string.gsub(request.uri, 'https?://(.-/)(.*)', '%1')
-	local text = "Hi " .. email .. ",\n"
-	text = text .. "Click this link to verify your account:\n"
-	text = text .. "https://" .. domain .. "/api/v1/verify/" .. ret;
-	Email.send({
-		from = 'Sample App <mail@exosite.com>',
-		to = email,
-		subject = ("Signup on " .. domain),
-		text = text
-	})
+  local domain = string.gsub(request.uri, 'https?://(.-/)(.*)', '%1')
+  local text = "Hi " .. email .. ",\n"
+  text = text .. "Click this link to verify your account:\n"
+  text = text .. "https://" .. domain .. "/api/v1/verify/" .. ret;
+  Email.send({
+    from = 'Sample App <mail@exosite.com>',
+    to = email,
+    subject = ("Signup on " .. domain),
+    text = text
+  })
 end
 
--- vim: set ai et sw=4 ts=4 :
+-- vim: set ai et sw=2 ts=2 :
