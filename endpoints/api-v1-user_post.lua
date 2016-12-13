a2394d77ee88fe49d394c00cdad110b40a7ca524
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2016-12-13 15:37:06 -0600

    block out body params

--- endpoints/api-v1-user_post.lua
+++ endpoints/api-v1-user_post.lua
@@ -1,24 +1,28 @@
 --#ENDPOINT POST /api/v1/user
 -- luacheck: globals request response (magic variables from Murano)
+local email = request.body.email
+local name = request.body.name
+local password = request.body.password
+
 local ret = User.createUser({
-	email = request.body.email,
-	name = request.body.name or request.body.email,
-	password = request.body.password
+	email = email,
+	name = name or email,
+	password = password
 })
 if ret.status_code ~= nil then
 	response.code = ret.status_code
 	response.message = ret.message
 else
 	local domain = string.gsub(request.uri, 'https?://(.-/)(.*)', '%1')
-	local text = "Hi " .. request.parameters.email .. ",\n"
+	local text = "Hi " .. email .. ",\n"
 	text = text .. "Click this link to verify your account:\n"
 	text = text .. "https://" .. domain .. "/api/v1/verify/" .. ret;
 	Email.send({
 		from = 'Sample App <mail@exosite.com>',
-		to = request.parameters.email,
+		to = email,
 		subject = ("Signup on " .. domain),
 		text = text
 	})
 end
 
--- vim: set ai sw=4 ts=4 :
+-- vim: set ai et sw=4 ts=4 :
