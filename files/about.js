692ee4ee7d58a2f332bf673e7b6c65786602dd5c
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2017-01-16 14:11:20 -0600

    Set home and about links; add about page.

---
+++ files/about.js
@@ -0,0 +1,43 @@
+$(function(){
+
+	var thisUser = null;
+
+	function signOut() {
+		// FIXME: This isn't deleteing.
+		Cookies.remove('sid', { Domain: window.location.hostname} );
+		window.location.href = 'login.html';
+	}
+
+	function checkSession() {
+		$.ajax({
+			method: 'GET',
+			url: '/api/v1/session',
+			success: function(data) {
+				// All good
+				thisUser = data;
+				$('.nav-signedin').show();
+				console.log("Is logged in");
+				$('#nav-signedin-message').html('Signed in as <b>' + data.name + '</b> ');
+				if ($.inArray('admin', data.roles) > -1) {
+					$('.needsAdmin').show();
+				}
+			},
+			error: function(xhr, textStatus, errorThrown) {
+				$('.nav-signedout').show();
+			}
+		});
+	}
+
+	$('.nav-signedin').hide();
+	$('.nav-signedout').hide();
+	$('#sign-out').click(function() {
+		signOut();
+	});
+	$('#sign-in').click(function() {
+		window.location.href = 'login.html';
+	});
+
+	checkSession();
+
+});
+//	vim: set sw=4 ts=4 :
